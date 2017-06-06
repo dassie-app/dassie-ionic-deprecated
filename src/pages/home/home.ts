f6e6912085ea0d24b1dce58c104bc1cc7e0392ea
Kyle Meenehan <kyle.meenehan@hellocomputer.com>
2017-06-06 14:40:13 +0200

    configured area page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { ApiService } from '../../app/api/api.service';
+
+import { AreaPage } from '../area/area';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  areas;
 
+  constructor(public navCtrl: NavController, private apiService: ApiService) {
+    this.areas = this.apiService.getAreasByRegion(1);
+  }
+
+  goToArea(id: number) {
+     this.navCtrl.push(AreaPage, {
+       id: id
+    });
   }
 
 }
