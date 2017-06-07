6bc90eaaf1621b95e5bdb02a6e50c3c3559f3ada
kylemeenehan <kylemeenehan@gmail.com>
2017-06-07 05:34:31 +0200

    added action sheet to crag view. styled routes list

--- src/pages/crag/crag.ts
+++ src/pages/crag/crag.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ActionSheetController } from 'ionic-angular';
 
 import { ApiService } from '../../app/api/api.service';
 
@@ -16,16 +16,66 @@ export class CragPage {
   crag;
   routes;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private apiService: ApiService) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private apiService: ApiService,
+    private actionSheetCtrl: ActionSheetController
+  ){
     this.cragId = this.navParams.get('id');
     this.crag = this.apiService.getCragById(this.cragId);
     this.routes = this.apiService.getRoutesByCrag(this.cragId);
+    this.routes.map(route=>{
+      route.starArray = [];
+      for (let i = 0; i < route.stars; i++){
+        route.starArray.push(0);
+      }
+    })
   }
 
   goToRoute(id: number) {
      this.navCtrl.push(RoutePage, {
        id: id
     });
+  }
+
+  openActionSheet(){
+    let actionSheet = this.actionSheetCtrl.create({
+      title: "Order Routes by",
+      buttons: [
+        {
+          text: 'Left to Right',
+          handler: ()=>{
+
+          }
+        },
+        {
+          text: 'Right to Left',
+          handler: ()=>{
+            
+          }
+        },
+        {
+          text: 'Grade: Low to High',
+          handler: ()=>{
+            
+          }
+        },
+        {
+          text: 'Grade: High to Low',
+          handler: ()=>{
+            
+          }
+        },
+        {
+          text: 'Star Rating',
+          handler: ()=>{
+            
+          }
+        }
+      ]
+    });
+    actionSheet.present();
   }
 
 }
