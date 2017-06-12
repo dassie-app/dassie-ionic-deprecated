22acdaf00bd4470c6fe63662a9d736bad4479930
Kyle Meenehan <kyle.meenehan@hellocomputer.com>
2017-06-12 16:41:44 +0200

    added star filter and fixed logic in filter

--- src/pages/search-routes/search-routes.ts
+++ src/pages/search-routes/search-routes.ts
@@ -15,7 +15,9 @@ export class SearchRoutesPage {
 
   routes;
   selectedGrade: any = 'any';
-  grades: any[] = ['any', 8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35];
+  selectedStars = 'any';
+  grades: any[] = [8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35];
+  stars : any[] = [1,2,3,4,5];
   searchTerm: string;
 
   constructor(
@@ -37,10 +39,6 @@ export class SearchRoutesPage {
      this.navCtrl.push(RoutePage, {
        id: id
     });
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SearchRoutesPage');
   }
 
 }
--- src/pipes/filter-routes/filter-routes.ts
+++ src/pipes/filter-routes/filter-routes.ts
@@ -6,13 +6,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FilterRoutesPipe implements PipeTransform {
   returnRoutes: any[];
   
-  transform(routes: any[], grade : any, searchTerm : string) {
+  transform(routes: any[], grade : any, searchTerm : string, stars: any) {
     this.returnRoutes = routes;
-    if (grade != 'any' || !grade){
+    if (grade != 'any' && grade){
       this.returnRoutes = this.returnRoutes.filter((route)=>{return route.grade === grade});
     }
     if (searchTerm) {
       this.returnRoutes = this.returnRoutes.filter((route)=>{return route.name.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1});
+    }
+    if (stars != 'any' && stars){
+      this.returnRoutes = this.returnRoutes.filter((route)=>{return route.stars === stars});
     }
 
     return this.returnRoutes;
