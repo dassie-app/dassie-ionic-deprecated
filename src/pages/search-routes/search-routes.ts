e4791b2bb6f003ffc6528f98d5dc0a824ba5303a
Kyle Meenehan <kyle.meenehan@hellocomputer.com>
2017-06-12 15:04:58 +0200

    added select for grades to the routes search page

--- src/pages/search-routes/search-routes.ts
+++ src/pages/search-routes/search-routes.ts
@@ -14,6 +14,8 @@ import { RoutePage } from '../route/route';
 export class SearchRoutesPage {
 
   routes;
+  selectedGrade: any = 'any';
+  grades: any[] = ['any', 8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35];
 
   constructor(
     public navCtrl: NavController,
