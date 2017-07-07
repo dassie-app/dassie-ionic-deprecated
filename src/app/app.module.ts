e9a96d945de50773a3de5bb054e6dfd2c6adb605
Kyle Meenehan <kylemeenehan@gmail.com>
2017-07-07 21:17:39 +0200

    imported angular fire into the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { IonicApp, IonicErrorHandler, IonicModule, NavController } from 'ionic-a
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { DeviceFeedback } from '@ionic-native/device-feedback';
-import { SQLite } from '@ionic-native/sqlite';
+import { AngularFireModule } from "angularfire2";
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { AreaPage } from '../pages/area/area';
 import { CragPage } from '../pages/crag/crag';
 import { RoutePage } from '../pages/route/route';
-import { SearchRoutesPage} from '../pages/search-routes/search-routes';
+import { SearchRoutesPage } from '../pages/search-routes/search-routes';
 
 import { MockDataService } from './api/mock-data.service';
 import { MockAreasService } from './api/mock-areas.service';
@@ -21,6 +21,15 @@ import { ApiService } from './api/api.service';
 
 import { SortRoutesPipe } from '../pipes/sort-routes/sort-routes';
 import { FilterRoutesPipe } from '../pipes/filter-routes/filter-routes';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAER4X8D21xgEAk3KJO2dvtkI249vIkw1w",
+  authDomain: "boven-route-guide.firebaseapp.com",
+  databaseURL: "https://boven-route-guide.firebaseio.com",
+  projectId: "boven-route-guide",
+  storageBucket: "boven-route-guide.appspot.com",
+  messagingSenderId: "302341661659"
+}
 
 @NgModule({
   declarations: [
@@ -35,7 +44,8 @@ import { FilterRoutesPipe } from '../pipes/filter-routes/filter-routes';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -50,13 +60,12 @@ import { FilterRoutesPipe } from '../pipes/filter-routes/filter-routes';
     StatusBar,
     SplashScreen,
     DeviceFeedback,
-    SQLite,
     ApiService,
     MockDataService,
     MockAreasService,
     MockCragsService,
     MockRoutesService,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
