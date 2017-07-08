ffb6b214295ebb1e5f9def7fc9fe62a9105fe80e
Kyle Meenehan <kylemeenehan@gmail.com>
2017-07-08 17:37:25 +0200

    added observables to the route view

--- src/pages/route/route.ts
+++ src/pages/route/route.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import {  NavController, NavParams } from 'ionic-angular';
+import { Component, OnDestroy } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { Subscription } from 'rxjs/subscription';
 
 import { ApiService } from '../../app/api/api.service';
 
@@ -10,7 +11,11 @@ import { AreaPage } from '../area/area';
   selector: 'page-route',
   templateUrl: 'route.html',
 })
-export class RoutePage {
+export class RoutePage implements OnDestroy {
+
+  routeSubscription: Subscription;
+  cragSubscription: Subscription;
+  areaSubscription: Subscription;
 
   routeId;
   route;
@@ -19,25 +24,40 @@ export class RoutePage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private apiService: ApiService) {
     this.routeId = this.navParams.get('id');
-    this.route = this.apiService.getRouteById(this.routeId);
-    this.crag = this.apiService.getCragById(this.route.crag);
-    this.area = this.apiService.getAreaById(this.crag.area);
-    this.route.starArray = [];
-    for (let i = 0; i < this.route.stars; i++){
-      this.route.starArray.push(0);
-    }
+
+    this.routeSubscription = this.apiService.getRouteById(this.routeId).subscribe((route) => {
+      this.route = route;
+      this.route.starArray = [];
+      for (let i = 0; i < this.route.stars; i++) {
+        this.route.starArray.push(0);
+      }
+    });
+
+    this.cragSubscription = this.apiService.getCragById(this.route.crag).subscribe((crag) => {
+      this.crag = crag;
+    });
+
+    this.areaSubscription = this.apiService.getAreaById(this.crag.area).subscribe((area) => {
+      this.area = area;
+    });
   }
 
   goToCrag(id: number) {
-     this.navCtrl.push(CragPage, {
-       id: id
+    this.navCtrl.push(CragPage, {
+      id: id
     });
   }
 
   goToArea(id: number) {
-     this.navCtrl.push(AreaPage, {
-       id: id
+    this.navCtrl.push(AreaPage, {
+      id: id
     });
+  }
+
+  ngOnDestroy() {
+    this.cragSubscription.unsubscribe();
+    this.routeSubscription.unsubscribe();
+    this.areaSubscription.unsubscribe();
   }
 
 }
